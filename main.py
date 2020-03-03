48dab003cd6811e0aa2ae590eeae448455c72442
hashimoto <hashimotorafa@gmail.com>
2020-03-03 00:58:41 -0300

    create main file

---
+++ main.py
@@ -0,0 +1,12 @@
+from tce_api.documentacao_de_informacoes_basicas.unidades_federativas import UnidadesFederativas
+from tce_api.documentacao_de_informacoes_basicas.municipios import Municipios
+from tce_api.documentacao_de_informacoes_basicas.orgaos import Orgaos
+
+# unidades_federativas = UnidadesFederativas()
+# unidades_federativas.execute()
+
+# municipios = Municipios()
+# municipios.execute()
+
+orgaos = Orgaos()
+orgaos.execute()
