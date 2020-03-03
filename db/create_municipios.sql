f723dc2b833c50139af4f6a00cc716afe2e3e8a9
hashimoto <hashimotorafa@gmail.com>
2020-03-02 23:07:30 -0300

    define yoyo as migration manager

--- db/create_municipios.sql
+++
@@ -1,7 +0,0 @@
-create table municipios (
-	id SERIAL PRIMARY KEY,
-	codigo varchar(10),
-	nome varchar(100),
-	codigo_unidade_federacao integer,
-	geoibge_id varchar(30)
-)
--- db/create_unidades_federacao.sql
+++
@@ -1,6 +0,0 @@
-create table unidades_federacao (
-	id SERIAL PRIMARY KEY,
-	codigo integer,
-	sigla varchar(10),
-	nome varchar(100)
-)
