2a0bb7cae15df62d3d534bb4da2113b142d2156b
hashimoto <hashimotorafa@gmail.com>
2020-04-05 02:50:11 -0300

    restructure request monitor table

--- migrations/20200303_04_y4n5m-create-tce-request-monitor.py
+++ migrations/20200303_04_y4n5m-create-tce-request-monitor.py
@@ -10,11 +10,15 @@ steps = [
     step(
         "create table tce_request_monitor ("
 	        "id SERIAL PRIMARY KEY,"
-	        "method varchar(10),"
+	        "method varchar(10) UNIQUE,"
+            "municipio_id integer,"
 	        "year integer,"
             "month integer,"
 	        "error varchar(255),"
-            "success boolean"
+            "success boolean,"
+            "tries integer,"
+            "created_at timestamp,"
+            "updated_at timestamp"
         ")",
         "DROP TABLE tce_request_monitor",
     )
