a1109be53c96687d7ec5c6f0f3d745d3b56de67d
hashimoto <hashimotorafa@gmail.com>
2020-04-05 03:02:02 -0300

    restructure unidade federativa model

--- model/unidade_federativa.py
+++ model/unidade_federativa.py
@@ -1,20 +1,18 @@
 from sqlalchemy import Column, Integer, String
 from model.base import Base
 
-class Municipio(Base):
+class UnidadeFederativa(Base):
     __tablename__ = 'unidades_federativas'
 
     id = Column(Integer, primary_key=True)
     codigo = Column(String)
     nome = Column(String)
-    codigo_unidade_federacao = Column(Integer)
-    geoibge_id = Column(String)
+    sigla = Column(Integer)
 
     def __init__(self, params):
-        self.codigo = params['codigo_municipio']
-        self.nome = params['nome_municipio']
-        self.geoibge_id = params['geoibgeId']
-        self.codigo_unidade_federacao = params['geoibgeId'][:2]
+        self.codigo = params['codigo_unidade_federacao']
+        self.nome = params['nome_unidade_federacao']
+        self.sigla = params['sigla_unidade_federacao']
 
     @classmethod
     def save_multiple(cls, array):
