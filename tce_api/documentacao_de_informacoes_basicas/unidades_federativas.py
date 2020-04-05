194dde0be31a8e6406316bccb58d9078817549e2
hashimoto <hashimotorafa@gmail.com>
2020-04-05 03:01:34 -0300

    refactor unidades federativas requester

--- tce_api/documentacao_de_informacoes_basicas/unidades_federativas.py
+++ tce_api/documentacao_de_informacoes_basicas/unidades_federativas.py
@@ -1,21 +1,18 @@
 from tce_api.base import Base
-import pdb
-import psycopg2
+from model.unidade_federativa import UnidadeFederativa
+
 class UnidadesFederativas(Base):
     def __init__(self):
         super().__init__()
-        self.cursor = self.db_connection.cursor()
-        self.base_method = 'unidades_federacao.json'
+        self.initialize_variables_by_method('unidades_federacao')
 
     def execute(self):
         try:
-            response = self.requests.get(self.base_url+self.base_method)
+            response = self.request_tce_api(self.method + '.json', '')
+            unidades_federativas = []
             for params in response.json()['rsp']['_content']:
-                self.cursor.execute(
-                    'INSERT INTO unidades_federativas (codigo, sigla, nome) VALUES(%s, %s, %s)',
-                    (params['codigo_unidade_federacao'], params['sigla_unidade_federacao'], params['nome_unidade_federacao'])
-                )
-            self.db_connection.commit()
-            self.cursor.close()
-        except psycopg2.DatabaseError as error:
-            print(error)
+                unidades_federativas.append(UnidadeFederativa(params))
+            UnidadeFederativa.save_multiple(unidades_federativas)
+            self.save_progress('', True)
+        except Exception as e:
+            self.save_progress(e, False)
