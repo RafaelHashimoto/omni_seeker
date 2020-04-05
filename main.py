714e84ad61c514af625249014d0b67ff3646e2c7
hashimoto <hashimotorafa@gmail.com>
2020-04-05 04:22:43 -0300

    refactor main

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+
 from tce_api.documentacao_de_informacoes_basicas.unidades_federativas import UnidadesFederativas
 from tce_api.documentacao_de_informacoes_basicas.municipios import Municipios
 from tce_api.documentacao_de_informacoes_basicas.orgaos import Orgaos
+from tce_api.documentacao_referente_a_processos_de_aquisicoes_e_contratos.licitacoes import Licitacoes
 
-# unidades_federativas = UnidadesFederativas()
-# unidades_federativas.execute()
-
-# municipios = Municipios()
-# municipios.execute()
-
-orgaos = Orgaos()
-orgaos.execute()
+UnidadesFederativas().execute()
+Municipios().execute()
+Orgaos().execute()
+Licitacoes().execute()
