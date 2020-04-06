33d2b277c92f86713e79fa4fbc7d7f7c26e4a5c3
hashimoto <hashimotorafa@gmail.com>
2020-04-06 01:02:51 -0300

    create gestores unidades gestoras resources

---
+++ migrations/20200406_04_2C1Pb-create-table-gestores-unidades-gestoras.py
@@ -0,0 +1,37 @@
+"""
+create table gestores_unidades_gestoras
+"""
+
+from yoyo import step
+
+__depends__ = {'20200406_03_8imYv-create-table-contas-extra-orcamentarias'}
+
+steps = [
+    step(
+        "create table gestores_unidades_gestoras ("
+            'id SERIAL PRIMARY KEY,'
+            'codigo_municipio varchar(10),'
+            'exercicio_orcamento varchar(10),'
+            'codigo_unidade_gestora varchar(10),'
+            'codigo_orgao varchar(10),'
+            'codigo_unidade varchar(10),'
+            'cpf_servidor varchar(50),'
+            'codigo_ingresso varchar(10),'
+            'codigo_vinculo varchar(10),'
+            'numero_expediente varchar(30),'
+            'data_inicio_gestao timestamp,'
+            'data_referencia varchar(10),'
+            'nome_gestor varchar(255),'
+            'data_fim_gestao varchar(10),'
+            'tipo_cargo varchar(10),'
+            'status_ordenador_despesa integer'
+        ")",
+        "DROP TABLE gestores_unidades_gestoras",
+    )
+]
+
+
+
+
+
+
---
+++ models/gestor_unidade_gestora.py
@@ -0,0 +1,49 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from models.base import Base
+
+class GestorUnidadeGestora(Base):
+    __tablename__ = 'gestores_unidades_gestoras'
+
+    id = Column(Integer, primary_key=True)
+    codigo_municipio = Column(String)
+    exercicio_orcamento = Column(String)
+    codigo_unidade_gestora = Column(String)
+    codigo_orgao = Column(String)
+    codigo_unidade = Column(String)
+    cpf_servidor = Column(String)
+    codigo_ingresso = Column(String)
+    codigo_vinculo = Column(String)
+    numero_expediente = Column(String)
+    data_inicio_gestao = Column(Datetime)
+    data_referencia = Column(String)
+    nome_gestor = Column(String)
+    data_fim_gestao = Column(String)
+    tipo_cargo = Column(String)
+    status_ordenador_despesa = Column(Integer)
+
+    def __init__(self, params):
+        self.codigo_municipio = params['codigo_municipio']
+        self.exercicio_orcamento = params['exercicio_orcamento']
+        self.codigo_unidade_gestora = params['codigo_unidade_gestora']
+        self.codigo_orgao = params['codigo_orgao']
+        self.codigo_unidade = params['codigo_unidade']
+        self.cpf_servidor = params['cpf_servidor']
+        self.codigo_ingresso = params['codigo_ingresso']
+        self.codigo_vinculo = params['codigo_vinculo']
+        self.numero_expediente = params['numero_expediente']
+        self.data_inicio_gestao = params['data_inicio_gestao']
+        self.data_referencia = params['data_referencia']
+        self.nome_gestor = params['nome_gestor']
+        self.data_fim_gestao = params['data_fim_gestao']
+        self.tipo_cargo = params['tipo_cargo']
+        self.status_ordenador_despesa = params['status_ordenador_despesa']
+
+
+	@classmethod
+	def save_multiple(cls, array):
+		cls.session.add_all(array)
+		cls.session.commit()
+
+	@classmethod
+	def all(cls):
+		return cls.session.query(cls).all()
