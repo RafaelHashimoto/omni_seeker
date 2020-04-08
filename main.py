1e9ba124e33092d16c8cdc3d11c341626baf5882
hashimoto <hashimotorafa@gmail.com>
2020-04-07 22:58:12 -0300

    call methods

--- main.py
+++ main.py
@@ -2,9 +2,26 @@
 from tce_api.documentacao_de_informacoes_basicas.unidades_federativas import UnidadesFederativas
 from tce_api.documentacao_de_informacoes_basicas.municipios import Municipios
 from tce_api.documentacao_de_informacoes_basicas.orgaos import Orgaos
+from tce_api.documentacao_de_informacoes_basicas.contas_bancarias import ContasBancarias
+from tce_api.documentacao_de_informacoes_basicas.tipos_unidades_administrativas import TiposUnidadesAdministrativas
+from tce_api.documentacao_de_informacoes_basicas.funcoes import Funcoes
+from tce_api.documentacao_de_informacoes_basicas.unidades_orcamentarias import UnidadesOrcamentarias
+from tce_api.documentacao_de_informacoes_basicas.contas_extra_orcamentarias import ContasExtraOrcamentarias
+from tce_api.documentacao_de_informacoes_basicas.unidades_gestoras import UnidadesGestoras
+from tce_api.documentacao_de_informacoes_basicas.gestores_unidades_gestoras import GestoresUnidadesGestoras
+from tce_api.documentacao_de_informacoes_basicas.ordenadores import Ordenadores
+
 from tce_api.documentacao_referente_a_processos_de_aquisicoes_e_contratos.licitacoes import Licitacoes
 
-UnidadesFederativas().execute()
-Municipios().execute()
-Orgaos().execute()
-Licitacoes().execute()
+# UnidadesFederativas().execute()
+# Municipios().execute()
+# Orgaos().execute()
+# ContasBancarias().execute()
+# TiposUnidadesAdministrativas().execute()
+# UnidadesOrcamentarias().execute()
+# ContasExtraOrcamentarias().execute()
+# UnidadesGestoras().execute()
+# GestoresUnidadesGestoras().execute()
+# Ordenadores().execute()
+# Funcoes().execute()
+# Licitacoes().execute()
