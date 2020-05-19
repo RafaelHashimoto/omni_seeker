c348bf364fa385395bfed4919902cd1b885c0654
hashimoto <hashimotorafa@gmail.com>
2020-05-19 00:39:56 -0300

    prepara licitacoes

--- tce_api/documentacao_referente_a_processos_de_aquisicoes_e_contratos/licitacoes.py
+++ tce_api/documentacao_referente_a_processos_de_aquisicoes_e_contratos/licitacoes.py
@@ -1,9 +1,10 @@
 from tce_api.base import Base
-from itertools import repeat
 from datetime import datetime
+from calendar import monthrange
 from models.licitacao import Licitacao
 from models.municipio import Municipio
 import pdb
+
 class Licitacoes(Base):
     def __init__(self):
         super().__init__()
@@ -16,19 +17,32 @@ class Licitacoes(Base):
                 licitacoes = []
                 for year in range(self.year, datetime.now().year):
                     self.year = year
-                    response = self.request_tce_api(self.url_with_params(municipio.codigo, year))
-                    for params in response['rsp']['_content']:
-                        licitacoes.append(Licitacao(params))
-                        Licitacao.save_multiple(licitacoes)
+                    if municipio.codigo in ['057', '099']:
+                        for month_range in self.months_range():
+                            response = self.request_tce_api(self.url_with_params(municipio.codigo, month_range))
+                            for params in response['rsp']['_content']:
+                                licitacoes.append(Licitacao(params))
+                    else:
+                        response = self.request_tce_api(self.url_with_params(municipio.codigo, self.year_range()))
+                        for params in response['rsp']['_content']:
+                            licitacoes.append(Licitacao(params))
+                Licitacao.save_multiple(licitacoes)
             self.save_progress('', True)
         except Exception as e:
             self.save_progress(e, False)
 
-    def url_with_params(self, code, year):
+    def months_range(self):
+        dates = []
+        for month in range(1, 12):
+            dates.append(str(self.year) + str(month).zfill(2)+ '01_' + str(self.year) + str(month).zfill(2) + str(monthrange(self.year, month)[1]).zfill(2))
+        return dates
+
+    def year_range(self):
+        return str(self.year) + '0101_' + str(self.year) + '1231'
+
+    def url_with_params(self, code, date):
         return (
                   '?codigo_municipio=' + code +
-                  '&exercicio_orcamento=' + str(year) +
-                  '&data_realizacao_autuacao_licitacao=' + str(year) + '0101_' + str(year) + '1231'
+                  '&exercicio_orcamento=' + str(self.year) +
+                  '&data_realizacao_autuacao_licitacao=' + date
                 )
-
-
