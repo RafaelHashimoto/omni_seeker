28d6473cc28383e6f7e552b0364dee243875fd06
hashimoto <hashimotorafa@gmail.com>
2020-05-19 23:50:51 -0300

    refatora requester

--- tce_api/by_city_requester.py
+++ tce_api/by_city_requester.py
@@ -1,27 +1,56 @@
 from tce_api.base import Base
 from datetime import datetime
 from models.municipio import Municipio
-
+import pdb
 class ByCityRequester(Base):
     def __init__(self, cls):
         super().__init__()
-        self.initialize_variables_by_method(cls.__table__.name)
         self.cls = cls
+        self.initialize_variables_by_method(self.cls.__table__.name)
+        self.array = []
 
     def execute(self):
         try:
             for municipio in Municipio.by_id_range(self.municipio_id):
                 self.municipio_id = municipio.id
-                array = []
+                self.array = []
                 for year in range(self.year, datetime.now().year):
                     self.year = year
-                    response = self.request_tce_api(self.url_with_params(municipio.codigo))
-                    for params in response['rsp']['_content']:
-                        array.append(self.cls(params))
-                self.cls.save_multiple(array)
+                    self.conditional_requester(municipio.codigo)
+                self.cls.save_multiple(self.array)
             self.save_progress('', True)
         except Exception as e:
             self.save_progress(e, False)
 
+    def conditional_requester(self, codigo):
+        if self.cls.__table__.name in self.table_name_list():
+            for month in range(1, 12):
+                self.insert_response_into_array(
+                    self.request_tce_api(
+                        self.url_with_params_by_data_referencia(codigo, month)
+                    )
+                )
+        else:
+            self.insert_response_into_array(
+                self.request_tce_api(self.url_with_params(codigo))
+            )
+
+    def insert_response_into_array(self, response):
+        for params in response['rsp']['_content']:
+            self.array.append(self.cls(params))
+
     def url_with_params(self, codigo):
         return ('?codigo_municipio=' + codigo + '&exercicio_orcamento=' + str(self.year))
+
+    def url_with_params_by_data_referencia(self, codigo, month):
+        return (
+            '?codigo_municipio=' + codigo + '&exercicio_orcamento=' + str(self.year) + '&data_referencia=' + str(self.year) + str(month).zfill(2)
+        )
+
+    def table_name_list(self):
+        return [
+            'balancete_despesa_extra_orcamentaria',
+            'balancete_receita_extra_orcamentaria',
+            'balancete_despesa_orcamentaria',
+            'balancete_receita_orcamentaria'
+        ]
